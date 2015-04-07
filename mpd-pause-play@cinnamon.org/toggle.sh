6708e684e0a0bc8392895a65c648f2a3fe86790b
christian <christian.steinhaus@rwth-aachen.de>
2015-04-07 12:49:35 +0200

    added toggle.sh

---
+++ mpd-pause-play@cinnamon.org/toggle.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SERVER="localhost";
+PASSWORD="";
+
+if [[ ! -z $1 ]];then
+	SERVER=$1;
+	if [[ ! -z $2 ]];then
+		PASSWORD=$2"@";
+	fi
+fi
+
+export MPD_HOST=$PASSWORD$SERVER
+mpc status
+exit 0
